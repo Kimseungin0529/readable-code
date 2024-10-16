f522ef161b87c2df67184a11eb3b6494114eaec5
김승진 <kimseungjin@gimseungjin-ui-MacBookPro.local>
2024-10-16 15:42:13 +0900

    test : CellPositions subtract 테스트 작성

--- src/test/java/cleancode/minesweeper/tobe/minesweeper/board/position/CellPositionsTest.java
+++ src/test/java/cleancode/minesweeper/tobe/minesweeper/board/position/CellPositionsTest.java
@@ -3,20 +3,60 @@ package cleancode.minesweeper.tobe.minesweeper.board.position;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CellPositionsTest {
-    CellPositions cellPositions;
 
-    @DisplayName("")
+    @DisplayName("지정된 개수만큼의 셀 위치를 임의로 추출한다")
     @Test
-    void test() {
+    void extractRandomPositions() {
         // given
+        List<CellPosition> cellPositionList = List.of(
+            CellPosition.of(0, 0),
+            CellPosition.of(0, 1),
+            CellPosition.of(2, 3),
+            CellPosition.of(4, 2),
+            CellPosition.of(1, 7)
+        );
+
+        CellPositions cellPositions = CellPositions.of(cellPositionList);
+        int count = 3;
+
         // when
+        List<CellPosition> randomPositions = cellPositions.extractRandomPositions(count);
+
         // then
+        assertThat(randomPositions).hasSize(count);
+        assertThat(cellPositionList).containsAll(randomPositions);
     }
+
+    @DisplayName("지정된 셀 위치들을 제외한 셀 위치 목록을 반환한다.")
+    @Test
+    void subtract() {
+        // given
+        List<CellPosition> cellPositionList = List.of(
+            CellPosition.of(0, 0),
+            CellPosition.of(0, 1),
+            CellPosition.of(2, 3),
+            CellPosition.of(4, 2),
+            CellPosition.of(1, 7)
+        );
+
+        List<CellPosition> subCellPositionList = cellPositionList.subList(0, 3);
+        CellPositions cellPositions = CellPositions.of(cellPositionList);
+        int size = cellPositionList.size() - subCellPositionList.size();
+
+        // when
+        List<CellPosition> subtractedCellPositions = cellPositions.subtract(subCellPositionList);
+
+        // then
+        assertThat(subtractedCellPositions)
+            .hasSize(size)
+            .doesNotContainAnyElementsOf(subCellPositionList);
+
+    }
+
 
 }
