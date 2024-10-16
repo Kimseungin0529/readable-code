c62e2f8d43e241f9b7051b1a32ddaed793d3bd63
김승진 <kimseungjin@gimseungjin-ui-MacBookPro.local>
2024-10-16 15:16:12 +0900

    test : CellPosition calculatePositionBy 해피 케이스, 예외 케이스 테스트 작성

--- src/test/java/cleancode/minesweeper/tobe/minesweeper/board/position/CellPositionTest.java
+++ src/test/java/cleancode/minesweeper/tobe/minesweeper/board/position/CellPositionTest.java
@@ -1,16 +1,14 @@
 package cleancode.minesweeper.tobe.minesweeper.board.position;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CellPositionTest {
-
 
 
     @DisplayName("셀의 위치를 나타내는 인덱스는 0보다 크거나 같아야 합니다.")
@@ -28,5 +26,42 @@ class CellPositionTest {
 
     }
 
+    @DisplayName("셀의 상대적인 위치를 계산한 결과는 예상된 좌표값이어야 한다")
+    @Test
+    void calculateRelativePositionCorrectly() {
+        // given
+        int rowIndex = 1;
+        int colIndex = 2;
+        CellPosition cellPosition = CellPosition.of(rowIndex, colIndex);
+
+        int deltaRow = 0;
+        int deltaCol = -1;
+        RelativePosition relativePosition = RelativePosition.of(deltaRow, deltaCol);
+
+        // when
+        CellPosition calculatedCellPosition = cellPosition.calculatePositionBy(relativePosition);
+
+        // then
+        assertThat(calculatedCellPosition.getRowIndex()).isEqualTo(rowIndex + deltaRow);
+        assertThat(calculatedCellPosition.getColIndex()).isEqualTo(colIndex + deltaCol);
+    }
+
+    @DisplayName("잘못된 상대적인 위치에 대한 셀을 계산할 수 없습니다.")
+    @Test
+    void throwExceptionCalculatePositionBy() {
+        // given
+        int rowIndex = 0;
+        int colIndex = 0;
+        CellPosition cellPosition = CellPosition.of(rowIndex, colIndex);
+
+        int deltaRow = 0;
+        int deltaCol = -1;
+        RelativePosition relativePosition = RelativePosition.of(deltaRow, deltaCol);
+
+        // when & then
+        assertThatThrownBy(() -> cellPosition.calculatePositionBy(relativePosition))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("움직일 수 있는 좌표가 아닙니다.");
+    }
 
 }
